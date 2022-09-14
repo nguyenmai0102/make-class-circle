63d41802f39ac72415963a44344256ab0ede109f
unknown <nguyenmai0102.meo@gmail.com>
2022-09-14 17:28:56 +0700

    noi dung

---
+++ Circle.js
@@ -0,0 +1,24 @@
+class Circle {
+    radius; color;
+
+    constructor(radius, color) {
+        this.radius = radius;
+        this.color = color;
+    }
+    setRadius(radius){
+        this.radius=radius
+    }
+    getRadius(){
+        return this.radius
+    }
+    setColor(color){
+        this.color=color
+    }
+    getColor(){
+       return  this.color
+    }
+    getArea(){
+
+        return this.radius*this.radius*Math.PI
+    }
+}
